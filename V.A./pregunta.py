9b956e9a3472610f56e21a3ddb5041f3160dfe06
EnriqueRZ <jramirez348@accitesz.com>
2019-03-28 15:44:41 -0600

    Respaldo

--- V.A./pregunta.py
+++ V.A./pregunta.py
@@ -4,11 +4,11 @@ from matplotlib import pyplot as plt
 
 def sobelOperator(img):
     container = np.copy(img)
-    size = container.shape
-    for i in range(1, size[0] - 1):
-        for j in range(1, size[1] - 1):
-            gx = (img[i - 1][j - 1] + 2*img[i][j - 1] + img[i + 1][j - 1]) - (img[i - 1][j + 1] + 2*img[i][j + 1] + img[i + 1][j + 1])
-            gy = (img[i - 1][j - 1] + 2*img[i - 1][j] + img[i - 1][j + 1]) - (img[i + 1][j - 1] + 2*img[i + 1][j] + img[i + 1][j + 1])
+    size = np.np.size(container, 0), np.size(container, 1)
+    for i in range(1, size[0]-1):
+        for j in range(1, size[1]-1):
+            gx = (img[i-1][j-1]+2*img[i][j-1]+img[i+1][j-1])-(img[i-1][j+1]+2*img[i][j+1]+img[i+1][j+1])
+            gy = (img[i-1][j-1]+2*img[i-1][j]+img[i-1][j+1])-(img[i+1][j-1]+2*img[i+1][j]+img[i+1][j+1])
             container[i][j] = min(255, np.sqrt(gx**2 + gy**2))
     return container
 
